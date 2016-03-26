dc831e36b8fd7b4a613eafbbaa567299e15fcc54
Cliff Hammerschmidt <cliff@enoki.ca>
2016-03-25 21:14:28 -0400

    fix hostname in example

--- es_watcher.sh
+++ es_watcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEAMCITY_HOST=teamcity.es
+
 trap ctrl_c INT
 function ctrl_c() {
   pkill nc
@@ -7,17 +9,17 @@ function ctrl_c() {
 }
 
 bash es_list_repos_and_branches.sh > x
-cat x | nc teamcity.rhi 3333
+cat x | nc ${TEAMCITY_HOST} 3333
 
 while :
 do
   echo -e "HTTP/1.1 200 OK\n\n" | nc -w 30 -l -p 3333 > /dev/null
   echo "checking"
-  cp x y)
+  cp x y
   bash es_list_repos_and_branches.sh > x
   if ! cmp x y >/dev/null 2>&1
   then
     echo updating
-    cat x | nc teamcity.rhi 3333
+    cat x | nc ${TEAMCITY_HOST} 3333
   fi
 done
