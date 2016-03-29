f255a16eb080cb6fd5c7f02ddf09fc70c966b1a0
Cliff Hammerschmidt <cliff@hammerschmidt.ca>
2016-03-29 14:27:57 -0700

    nc using -i for timeout on listen, not -w

--- es_watcher.sh
+++ es_watcher.sh
@@ -11,7 +11,7 @@ cat x | nc ${TEAMCITY_HOST} 3333
 
 while :
 do
-  echo -e "HTTP/1.1 200 OK\n\n" | nc -w 30 -l -p 3333 > /dev/null
+  echo -e "HTTP/1.1 200 OK\n\n" | nc -i 3 -l -p 3333 > /dev/null
   echo "checking"
   cp x y
   bash es_list_repos_and_branches.sh > x
