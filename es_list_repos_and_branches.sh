57ac2ae13543dd7dae50a341c5ef61cc4371b30c
Cliff Hammerschmidt <cliff@hammerschmidt.ca>
2016-04-01 14:40:00 -0700

    auto-generate build configs

--- es_list_repos_and_branches.sh
+++ es_list_repos_and_branches.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 pushd /var/opt/gitlab/git-data/repositories > /dev/null
 gitlaburl="git@${GITLAB_HOST}:"
 
@@ -17,9 +16,36 @@ function lsb {
     if [ $? != 0 ]
     then
       config="skip"
+    else
+      name=$(git show master:sln.vsg | grep name)
+      name=${name/%,/}
+      dotSettings=$(git cat-file -p $branch^{tree} | grep sln.DotSettings)
+      if [[ -n $dotSettings ]]
+      then
+        dotSettings="true"
+      else
+        dotSettings="false"
+      fi
+      testAssembly=$(git cat-file -p $branch^{tree} | grep tree | grep Test | while read l; do echo "${l:53}"; done)
+      if [[ -n $testAssembly ]]
+      then
+        testAssembly="true"
+      else
+        testAssembly="false"
+      fi
+      assemblies=$(git cat-file -p $branch^{tree} | grep tree | grep -v Test | while read l; do echo "${l:53}"; done)
+      config=${config/$$ /}
+      config=${config/$'{'/}
+      addconfig="{$name,\"assemblies\":[";
+      for assembly in $assemblies
+      do
+        addconfig="$addconfig\"$assembly\","
+      done
+      addconfig=${addconfig/%,/}
+      config="$addconfig],\"dotSettings\":$dotSettings,\"test\":$testAssembly,$config"
     fi
     echo "+BRANCH " $branch $version
-    echo $config
+    echo "$config"
     echo "-BRANCH "
   done
   echo "-REPO"
