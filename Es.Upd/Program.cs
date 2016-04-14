255cad4fa424307f7c156219425689c9089a7abf
Cliff Hammerschmidt <chammerschmidt@roadhouseinteractive.com>
2016-04-14 10:40:16 -0700

    wip

--- Es.Upd/Program.cs
+++ Es.Upd/Program.cs
@@ -5,9 +5,25 @@ namespace Es.Upd
 {
     public static class Program
     {
+        private static string _id;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", Assembly.GetExecutingAssembly().GetName().Name, BuildInfo.Version);
+            _id = Assembly.GetExecutingAssembly().GetName().Name;
+            Console.WriteLine("{0} {1}", _id, BuildInfo.Version);
+
+            // internal:
+            //  install
+            //  enable
+            //  disable
+            //  set default
+            //
+            // external:
+            //  locate X (optional: label)
+            //   -> ( relative /name/vx.../ or abs http://host/name/vz.../ ) + CDN + signed(expiry in seconds from epoch)
+            //   
+            //
+            //
         }
     }
 }
